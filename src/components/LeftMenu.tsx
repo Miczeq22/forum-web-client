0a370e1e33d2e36c35887df4797a1fa5ae2f4ba4
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-07-02 21:22:49 +0200

    ✨ Fetch categories

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import Category from "../models/Category";
+import { getCategories } from "../services/DataService";
 
 export const LeftMenu = () => {
   const { width } = useWindowDimensions();
+  const [categories, setCategories] = useState<JSX.Element>(
+    <div>Left Menu</div>
+  );
+
+  useEffect(() => {
+    getCategories()
+      .then((categories: Array<Category>) => {
+        const cats = categories.map((cat) => <li key={cat.id}>{cat.name}</li>);
+
+        setCategories(<ul className="category">{cats}</ul>);
+      })
+      .catch(console.log);
+  }, []);
 
   if (width <= 768) {
     return null;
   }
 
-  return <div className="leftmenu">Left Menu</div>;
+  return <div className="leftmenu">{categories}</div>;
 };
