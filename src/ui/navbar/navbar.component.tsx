376f8d73a759aff18fa05a1a69e8188889786852
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-07-04 18:36:20 +0200

    🌈 Add hamburger menu

--- src/ui/navbar/navbar.component.tsx
+++ src/ui/navbar/navbar.component.tsx
@@ -1,10 +1,25 @@
-export const Navbar = () => (
-  <div
-    style={{
-      gridArea: "nav",
-      backgroundColor: "red",
-    }}
-  >
-    Navbar
-  </div>
-);
+import { Button, useMantineTheme } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
+import { FaBars } from "react-icons/fa";
+
+export const Navbar = () => {
+  const theme = useMantineTheme();
+
+  const { width } = useViewportSize();
+
+  return (
+    <div
+      style={{
+        gridArea: "nav",
+        backgroundColor: "red",
+      }}
+    >
+      {width <= theme.breakpoints.sm && (
+        <Button variant="subtle">
+          <FaBars />
+        </Button>
+      )}
+      Navbar
+    </div>
+  );
+};
