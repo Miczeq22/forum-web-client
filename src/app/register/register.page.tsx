4dcbe0c7238574716ddc08fe4cdebd6f9a3d85a0
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-07-12 21:16:25 +0200

    ✨ Register page init

---
+++ src/app/register/register.page.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "../../theme/layouts/main.layout"
+
+export const RegisterPage = () => {
+    return (
+        <MainLayout>
+            Register
+        </MainLayout>
+    )
+}
