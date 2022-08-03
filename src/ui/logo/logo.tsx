de341c41c41bb3b020d017b9c8b68781009ac82a
Mikołaj Wargowski <mikoajw20@gmail.com>
2022-08-03 19:10:33 +0200

    🌈 Change logo size depending on screen size

--- src/ui/logo/logo.tsx
+++ src/ui/logo/logo.tsx
@@ -1,8 +1,10 @@
 import { Anchor, Title, useMantineTheme } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 import { Link } from "react-router-dom";
 
 export const Logo = () => {
   const theme = useMantineTheme();
+  const { width } = useViewportSize();
 
   return (
     <Anchor underline={false} to="/#" component={Link}>
@@ -11,6 +13,10 @@ export const Logo = () => {
         sx={{
           fontFamily: "Dancing Script",
           color: theme.colors.orange[8],
+          fontSize:
+            width <= theme.breakpoints.sm
+              ? theme.fontSizes.xl
+              : 27,
         }}
       >
         My Forum
